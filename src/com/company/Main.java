9ba5c26d4bbe2fc0fccfef6acc6b09a2944499d3
tkurudi <t.kurudi209@gmail.com>
2019-09-23 17:00:27 +1200

    challange

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,22 +11,51 @@ public class Main {
         int levelCompleted = 5;
         int bonus = 100;
 
-        calculateScore(true, 800, levelCompleted, bonus);
+        int highScore = calculateScore(true, 800, levelCompleted, bonus);
 
-        calculateScore(true, 10000, 8, 200);
+       highScore = calculateScore(true, 10000, 8, 200);
 
+        int position = calculateHighScorePosition(1500);
+        displayHighScorePosition("tim", position);
 
+        position = calculateHighScorePosition(900);
+        displayHighScorePosition("bob", position);
+        position = calculateHighScorePosition(400);
+        displayHighScorePosition("fred", position);
+        position = calculateHighScorePosition(50);
+        displayHighScorePosition("jim", position);
         }
 
 
-    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
 
 
         if(gameOver) {
             int finalScore = score + (levelCompleted * bonus);
-            finalScore += 1000; 
+            finalScore += 1000;
             System.out.println("Your final score was " + finalScore);
-
+            return finalScore;
         }
+            return -1;
+    }
+
+    public static void displayHighScorePosition(String playerName, int position ) {
+
+        System.out.println(playerName + "  managed to get into position  " +
+                position + "  on the high score table  "
+                );
+    }
+
+    public static int calculateHighScorePosition(int playerScore){
+        if(playerScore >= 1000) {
+            return  1;
+        }else if(playerScore >= 500 && playerScore < 1000) {
+            return 2;
+        } else if(playerScore >= 100 && playerScore < 500) {
+            return 3;
+        } else {
+            return 4;
+        }
+
     }
 }
